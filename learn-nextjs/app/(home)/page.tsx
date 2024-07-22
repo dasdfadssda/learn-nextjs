509bd360b0d87d17e2a702256168ac31e09ce940
dasdfadssda <quddkflty@gmail.com>
2024-07-23 01:07:16 +0900

    route dynamic

---
+++ learn-nextjs/app/(home)/page.tsx
@@ -0,0 +1,5 @@
+export default function Tomato() {
+    return <div>
+        <h1>Hello</h1>
+    </div>
+}
--- learn-nextjs/app/about-us/page.tsx
+++ learn-nextjs/app/about-us/page.tsx
@@ -1,5 +1,3 @@
-import Navigation from "../../components/navigation";
-
 export default function About (){
     return <div>
         <h1>about - us!</h1>
