e0cb613fbf76c0f47fbb6ec2fa6a451a86575701
dasdfadssda <quddkflty@gmail.com>
2024-07-23 14:26:24 +0900

    api fetch & loading

--- learn-nextjs/app/(home)/page.tsx
+++ learn-nextjs/app/(home)/page.tsx
@@ -1,5 +1,30 @@
-export default function Tomato() {
-    return <div>
-        <h1>Hello</h1>
-    </div>
+import Link from "next/link";
+
+export const metadata = {
+    title: "home"
+}
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+// 영화 목록을 가져오는 비동기 함수
+async function GetMovies() {
+    console.log("fetching start");
+    const response = await fetch(API_URL, { next: { revalidate: 60 } }); // 60초 동안 캐싱
+    const json = await response.json();
+    return json; // JSON 형식으로 응답 반환
+}
+
+// 홈 페이지 컴포넌트: 영화 목록을 가져와서 렌더링
+export default async function HomePage() {
+    const movies = await GetMovies(); // 영화 목록을 비동기적으로 가져옴
+
+    return (
+        <div>
+            {movies.map(movie => (
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link> // 각 영화에 대한 링크 렌더링
+                </li>
+            ))}
+        </div>
+    );
 }
--- learn-nextjs/app/(movies)/movies/[id]/page.tsx
+++ learn-nextjs/app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,26 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+// 특정 ID에 대한 영화 정보를 가져오는 비동기 함수
+async function getMovie(id: string) {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 지연
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json(); // JSON 형식으로 응답 반환
+}
+
+// 특정 ID에 대한 비디오 정보를 가져오는 비동기 함수
+async function getVideos(id: string) {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 지연
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json(); // JSON 형식으로 응답 반환
+}
+
+// 페이지 컴포넌트: 영화 및 비디오 정보를 병렬로 가져와 렌더링
+export default async function MovieDetail({
     params: { id },
 }: {
-    params: { id: string }
-}
-) {
-    return <div>
-        <h1>Movie {id}</h1>
-    </div>
+    params: { id: string };
+}) {
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]); // 두 API 요청을 병렬로 실행
+
+    return <h1>{movie.title}</h1>; // 영화 제목을 렌더링
 }
