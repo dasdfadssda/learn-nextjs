d64def97111be04346c7721726b00a3bd050c3d1
dasdfadssda <quddkflty@gmail.com>
2024-07-23 16:20:04 +0900

    meta data 설정

--- learn-nextjs/app/(movies)/movies/[id]/page.tsx
+++ learn-nextjs/app/(movies)/movies/[id]/page.tsx
@@ -1,21 +1,28 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-// 페이지 컴포넌트: 영화 및 비디오 정보를 병렬로 가져와 렌더링
-export default async function MovieDetail({
-    params: { id },
-}: {
-    params: { id: string };
-}) {
-    return (
-        <div>
-            <Suspense fallback={<h1>Loading movie info</h1>}>
-                <MovieInfo id={id} /> {/* 영화 정보 컴포넌트 */}
-            </Suspense>
-            <Suspense fallback={<h1>Loading movie video</h1>}>
-                <MovieVideos id={id} /> {/* 영화 비디오 컴포넌트 */}
-            </Suspense>
-        </div>
-    );
+interface IParams {
+  params: { id: string };
+}
+
+ // 메타 데이터 제목으로 설정
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
--- learn-nextjs/components/movie-info.tsx
+++ learn-nextjs/components/movie-info.tsx
@@ -1,7 +1,7 @@
-import style from "../styles/movie-info.module.css";
+import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../app/(home)/page";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
@@ -9,15 +9,15 @@ async function getMovie(id: string) {
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
   return (
-    <div className={style.container}>
+    <div className={potato.container}>
       <img
         src={movie.poster_path}
-        className={style.poster}
+        className={potato.poster}
         alt={movie.title}
       />
-      <div className={style.info}>
-        <h1 className={style.title}>{movie.title}</h1>
-        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+      <div className={potato.info}>
+        <h1 className={potato.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed()}</h3>
         <p>{movie.overview}</p>
         <a href={movie.homepage} target={"_blank"}>
           Homepage &rarr;
